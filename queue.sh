b952e2545b472edc923b1d8a23fb931bce2f7737
Daniel Dowling <daniel.pierre.dowling@gmail.com>
2023-12-22 10:56:18 +0800

    Merged process_queue.sh into queue.sh as exported function.

--- queue.sh
+++ queue.sh
@@ -34,9 +34,28 @@ cleanup() {
 	rm -rf $daemontmp
 }
 
-
 trap cleanup EXIT
+#Process queue subroutine
+process_queue() {
+	local cmd=$(echo $1 | sed 's|^[0-9]\+ ||')
+	local pid=$(echo $1 | sed 's|^\([0-9]\+\) .*$|\1|')
+	echo -e "$(date +%b\ %e\ %T) [$pid]:\t$cmd" >&2
+	if [ ! -d "/proc/$pid" ]
+	then
+		echo -e "$(date +%b\ %e\ %T) [$pid]:\t\033[0;31mProcess terminated before execution.\033[0m" >&2
+		exit 999
+	fi
+	#hook up file descriptors to invoking process (as well as fd4 for retval)
+	exec 0<>"/proc/$pid/fd/0"
+	exec 1>"/proc/$pid/fd/1"
+	exec 2>"/proc/$pid/fd/2"
+	exec 4>"/proc/$pid/fd/4"
+	eval $cmd <&0 
+	echo -e "$?"'\0' >&4
+	#echo "retval transmitted"
+}
+export -f process_queue
 
 exec 9<> "$daemontmp/queue_fifo"
 echo "Starting process loop" >&2
-xargs -d '\n' -P$threads -I## /home/sentient/process_queue.sh '##' <&9
+xargs -d '\n' -P$threads -I## bash -c "process_queue '##'" <&9
